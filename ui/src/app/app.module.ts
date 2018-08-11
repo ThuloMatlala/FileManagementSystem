fb809d313c4c391efd879bf09486be50d940ab19
Thulo Maepa <Thulo@Thulos-MacBook-Pro.local>
2018-08-11 22:02:21 +0200

    Added bottom nav panel

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MainPanelComponent } from './main-panel/main-panel.component';
+import { BottomPanelComponent } from './bottom-panel/bottom-panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    MainPanelComponent
+    MainPanelComponent,
+    BottomPanelComponent
   ],
   imports: [
     BrowserModule
