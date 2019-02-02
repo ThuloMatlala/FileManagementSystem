09b34d12982dbe969368e66b4f1b87a831ca34ff
Thulo Maepa <thulo.maepa@krs.co.za>
2019-02-02 14:26:00 +0200

    'WIP'

--- userManagement/src/main/java/com/tprice/userManagement/model/Client.java
+++ userManagement/src/main/java/com/tprice/userManagement/model/Client.java
@@ -1,6 +1,21 @@
 package com.tprice.userManagement.model;
-import lombok.Data;
-public class Client
-{
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+class Client
+{
+  private @Id @GeneratedValue Long id;
+  private String username;
+  private String password;
+
+  private Client (String username, String password){
+      this.username = username;
+      this.password = password;
+  }
 }
