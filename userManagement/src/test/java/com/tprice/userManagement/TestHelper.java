42d23f9b50502dc02df969d65c5d5ccb0bbf927a
Thulo Maepa <thulo.maepa@krs.co.za>
2019-04-16 18:55:48 +0200

    Added test data t o testhelper

--- userManagement/src/test/java/com/tprice/userManagement/TestHelper.java
+++ userManagement/src/test/java/com/tprice/userManagement/TestHelper.java
@@ -18,4 +18,54 @@ public class TestHelper {
         userDetails.put("cdibGrade", "Test CDIB GRADE");
         return userDetails;
     }
+
+    public JSONObject AddMultipleUsers() throws JSONException
+    {
+        JSONObject userDetails = new JSONObject();
+        JSONObject userObject = new JSONObject();
+        userDetails.put("id", 1);
+        userDetails.put("firstName", "Test First Name");
+        userDetails.put("lastName", "Test Last Name");
+        userDetails.put("phone", "+00(111)-222-3333");
+        userDetails.put("position", "Test Position");
+        userDetails.put("companyName", "Test Company");
+        userDetails.put("tradingName", "Test Trading Name");
+        userDetails.put("cdibGrade", "Test CDIB GRADE");
+        userObject.put("user", userDetails);
+
+
+        userDetails.put("id", 2);
+        userDetails.put("firstName", "2Test First Name");
+        userDetails.put("lastName", "2Test Last Name");
+        userDetails.put("phone", "2+00(111)-222-3333");
+        userDetails.put("position", "2Test Position");
+        userDetails.put("companyName", "2Test Company");
+        userDetails.put("tradingName", "2Test Trading Name");
+        userDetails.put("cdibGrade", "2Test CDIB GRADE");
+        userObject.put("user", userDetails);
+
+
+        userDetails.put("id", 3);
+        userDetails.put("firstName", "3Test First Name");
+        userDetails.put("lastName", "3Test Last Name");
+        userDetails.put("phone", "3+00(111)-222-3333");
+        userDetails.put("position", "3Test Position");
+        userDetails.put("companyName", "3Test Company");
+        userDetails.put("tradingName", "3Test Trading Name");
+        userDetails.put("cdibGrade", "3Test CDIB GRADE");
+        userObject.put("user", userDetails);
+
+
+        userDetails.put("id", 4);
+        userDetails.put("firstName", "4Test First Name");
+        userDetails.put("lastName", "4Test Last Name");
+        userDetails.put("phone", "4+00(111)-222-3333");
+        userDetails.put("position", "4Test Position");
+        userDetails.put("companyName", "4Test Company");
+        userDetails.put("tradingName", "4Test Trading Name");
+        userDetails.put("cdibGrade", "4Test CDIB GRADE");
+        userObject.put("user", userDetails);
+
+        return userObject;
+    }
 }
