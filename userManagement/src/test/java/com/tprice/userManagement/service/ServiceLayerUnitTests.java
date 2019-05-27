1fe7f7dd768525b6307184bbcfa7aea8b0680ca7
Thulo Maepa <thulo.maepa@krs.co.za>
2019-05-27 10:51:47 +0200

    added ServiceLayerUnitTests file with getUser test to get junit working. GetUserTestFailing

---
+++ userManagement/src/test/java/com/tprice/userManagement/service/ServiceLayerUnitTests.java
@@ -0,0 +1,34 @@
+package com.tprice.userManagement.service;
+
+import com.tprice.userManagement.model.User;
+import com.tprice.userManagement.repo.UserRepo;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.junit.Assert.*;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class ServiceLayerUnitTests {
+
+    @Autowired
+    private UserService userService;
+
+    @MockBean
+    private UserRepo userRepo;
+
+    @Test
+    public void getUsers(){
+        when(userRepo.findAll()).thenReturn(Stream.of(new User()).collect(Collectors.toList()));
+        Assert.assertEquals(1, userService.getAllUsers().size());
+    }
+}
