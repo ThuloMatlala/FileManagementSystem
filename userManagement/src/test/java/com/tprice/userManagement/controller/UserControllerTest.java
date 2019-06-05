036a4e8511bcd4a17a72d0d66de642c3bc47346d
Thulo Maepa <thulo.maepa@krs.co.za>
2019-06-05 05:54:00 +0200

    clean up imports

--- userManagement/src/test/java/com/tprice/userManagement/controller/UserControllerTest.java
+++ userManagement/src/test/java/com/tprice/userManagement/controller/UserControllerTest.java
@@ -5,25 +5,19 @@ import com.tprice.userManagement.TestHelper;
 import com.tprice.userManagement.model.User;
 import com.tprice.userManagement.service.UserService;
 import org.hamcrest.Matchers;
-import org.hamcrest.collection.IsCollectionWithSize;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.client.match.MockRestRequestMatchers;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.List;
 
-import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
-import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -61,7 +55,7 @@ public class UserControllerTest {
 
 
     @Test
-    public void getAllUsers() throws Exception {
+        public void getAllUsers() throws Exception {
         List<User> userList = testHelper.CreateMultipleUsers();
 
         when(userService.findAllUsers()).thenReturn(userList);
