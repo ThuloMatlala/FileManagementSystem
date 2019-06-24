e87d841095e300e9a76c941c5ffd6542ee71f00c
Thulo Maepa <thulomaepa@gmail.com>
2019-06-24 21:33:26 +0200

    added GetAllUsersShouldReturnAnEmptyListOfUsers test passing

--- userManagement/src/test/java/com/tprice/userManagement/repository/UserIntegrationTests.java
+++ userManagement/src/test/java/com/tprice/userManagement/repository/UserIntegrationTests.java
@@ -1,34 +1,23 @@
 package com.tprice.userManagement.repository;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.tprice.userManagement.TestHelper;
 import com.tprice.userManagement.UserManagementApplication;
-import com.tprice.userManagement.model.User;
-import com.tprice.userManagement.repo.UserRepo;
-import com.tprice.userManagement.service.UserService;
-import org.junit.Before;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.assertj.core.api.Assertions.*;
-
-
-import java.util.List;
-import java.util.Optional;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(
@@ -43,13 +32,19 @@ public class UserIntegrationTests {
     MockMvc mockMvc;
 
     @Test
-    public void contextLoads() throws Exception
+    public void SmokeScreenTest() throws Exception
     {
-
-        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("api/test")
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/test")
                 .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
                 .andReturn();
+    }
 
-        System.out.println(mvcResult);
+    @Test
+    public void GetAllUsersShouldReturnAnEmptyListOfUsers() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/users")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
     }
 }
